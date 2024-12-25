d6aa42092e01a63ffa0a195473ea6dcd5b5e0a17
Suhga Al Amin <developersuhag@gmail.com>
2024-12-25 15:19:12 +0600

    newsletter

---
+++ src/components/ui/home/Newsletter.jsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+
+const Newsletter = () => {
+  return (
+    <Container className=" home_newsletter">
+      <h4>Stay up to Date </h4>
+      <p>
+        Subscribe to Innovat’s Monthly Research & Insights Newsletter Get
+        exclusive updates and notifications on Innovat’s latest research and
+        insights to keep you ahead in a fast-changing global economy.
+      </p>
+      <button>Subscribe</button>
+    </Container>
+  );
+};
+
+export default Newsletter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Education from "../components/ui/home/Education";
 import Hero from "../components/ui/home/Hero";
 import InvestmentFunds from "../components/ui/home/InvestmentFunds";
+import Newsletter from "../components/ui/home/Newsletter";
 import OurApproach from "../components/ui/home/OurApproach";
 import ResearchCenter from "../components/ui/home/ResearchCenter";
 import "../styles/home.scss";
@@ -13,6 +14,7 @@ const Home = () => {
       <InvestmentFunds />
       <Education />
       <ResearchCenter />
+      <Newsletter />
     </div>
   );
 };
